8d2e6bf4cb96f76e219f8b202d4a6374585bb7f5
Donat <hundonop@gmail.com>
2019-09-24 10:23:12 +0200

    core struktúra építgetés

---
+++ server/core/momentum.cpp
@@ -0,0 +1,6 @@
+#include "momentum.h"
+
+Momentum::Momentum()
+{
+
+}
---
+++ server/core/momentum.h
@@ -0,0 +1,11 @@
+#ifndef MOMENTUM_H
+#define MOMENTUM_H
+
+
+class Momentum
+{
+public:
+    Momentum();
+};
+
+#endif // MOMENTUM_H

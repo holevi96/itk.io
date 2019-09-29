84019bb91f4faf0120025467cb89772056ded788
Donat <hundonop@gmail.com>
2019-09-29 20:02:05 +0200

    game core nagyon váza kész

---
+++ server/core/acceleration.h
@@ -0,0 +1,11 @@
+#ifndef ACCELERATION_H
+#define ACCELERATION_H
+
+
+class Acceleration
+{
+public:
+    Acceleration();
+};
+
+#endif // ACCELERATION_H
--- server/core/momentum.cpp
+++
@@ -1,6 +0,0 @@
-#include "momentum.h"
-
-Momentum::Momentum()
-{
-
-}

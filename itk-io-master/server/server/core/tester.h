8a4949127e34ede8f1bf1205e634c376883264df
Donat <hundonop@gmail.com>
2019-11-26 15:23:55 +0100

    Fallal is ütköznek a hajók

--- itk-io-master/server/server/core/tester.h
+++ itk-io-master/server/server/core/tester.h
@@ -21,6 +21,16 @@ private:
     int msNum=0;
     int timeStep;
     GameCore gc;
+    void test01();
+    void test02();
+    void test03();
+    void test04();
+    void test05();
+    void test06();
+    void test07();
+    void test08();
+    void test09();
+    void test10();
 };
 
 #endif // TESTER_H

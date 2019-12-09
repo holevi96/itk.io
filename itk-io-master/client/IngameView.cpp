c9153ec29d3cb7ff34ae270363c1a432648f9f4e
Levi <holevi96@gmail.com>
2019-12-09 01:42:57 +0100

    wip

--- itk-io-master/client/IngameView.cpp
+++ itk-io-master/client/IngameView.cpp
@@ -10,9 +10,15 @@ IngameView::IngameView(MainWindow* w, QStackedWidget* st) : QWidget(st),window(w
     view=new QGraphicsView(this);
     scene=new QGraphicsScene(this);
 
+
     QPixmap pim(QPixmap(":/images/background.jpg"));
 
     scene->setSceneRect(0,0,window->width()-110,window->height()-110);
+    const QPen *qp = new QPen(Qt::red, 30, Qt::SolidLine,Qt::SquareCap, Qt::RoundJoin);
+
+     scene->addLine(0,0,0,window->getGameData().serverInfo.sizeY,*qp);
+     QGraphicsLineItem* topLeftBorder = new QGraphicsLineItem(0,0,0,window->height()-110);
+
     view->setFixedSize(window->width()-120,window->height()-50);
     scene->setBackgroundBrush(pim.scaled(window->width()-110,window->height()-110,Qt::IgnoreAspectRatio,Qt::SmoothTransformation));
     //view->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOn);
